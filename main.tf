7d08fa4136e966371dd0f9c2449b4e49008aaf06
Srinath Balne <srinath.balne@alight.com>
2022-08-18 13:09:46 +0530

    git test

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "db_server" {
+  ami           = "ami-090fa75af13c156b4"
+  instance_type = "t2.micro"
+  count = 2
+
+  tags = {
+    Name = "examplemultipleservers"
+  }
+}
